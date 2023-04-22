6fbf3a53e4a8dbe6d671bdb4ef04f2e0d4971ba9
Marcin Bartmiński <mbdjj@o2.pl>
2023-04-22 10:27:16 +0200

    :tada: Create Solar System model in RealityKit

--- solar-system/ContentView.swift
+++ solar-system/ContentView.swift
@@ -20,11 +20,9 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let solarSystemAnchor = try! SolarSystem.load_SolarSystem()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(solarSystemAnchor)
         
         return arView
         
